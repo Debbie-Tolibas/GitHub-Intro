48a0ab45a0e4ce75ee44dc69d056a9485cb15eef
Debbie <dtolibas.k11935758@umak.edu.ph>
2024-10-12 01:06:07 +0000

    Modified hello.py

--- hello.py
+++ hello.py
@@ -2,3 +2,19 @@ Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 print("Hello, World!")
 Hello, World!
+name = input("Enter your name: ")
+print(f"Hello, {name}!")
+SyntaxError: multiple statements found while compiling a single statement
+ name = input("Enter your name: ")
+    print("Hello, " + name + "!")
+    
+SyntaxError: unexpected indent
+name = input("Enter your name: ")
+    print("Hello, " + name + "!")
+    
+SyntaxError: multiple statements found while compiling a single statement
+name = input("Enter your name: ") print("Hello, " + name + "!")
+SyntaxError: invalid syntax
+name = input("Enter your name: "); print("Hello, " + name + "!")
+Enter your name: Debbie
+Hello, Debbie!
